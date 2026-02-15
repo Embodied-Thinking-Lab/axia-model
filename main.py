188b591e229500bd9a495d1cd0ec84264315ec84
44David <david.soane@outlook.com>
2026-02-14 21:55:30 -0600

    create sim env

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from axia-model!")
+import gymnasium as gym
+import panda_gym
 
+env = gym.make("PandaReach-v3", render_mode="rgb_array", renderer="Tiny")
+obs, info = env.reset()
 
-if __name__ == "__main__":
-    main()
+print("obs space:", env.observation_space)
+print("action space:", env.action_space)
+print("obs keys:", obs.keys() if isinstance(obs, dict) else "Not dict")
+
+img = env.render()
+action = env.action_space.sample()
+obs, reward, terminated, truncated, info = env.step(action)
+
+print(reward)
