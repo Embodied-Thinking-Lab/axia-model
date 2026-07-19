96226ef70a8bc35c710823d66534ce613384a8b8
44David <david.soane@outlook.com>
2026-07-18 22:58:27 -0600

    implement state encoder and dynamics model for new TD-MPC2 architecture

---
+++ model.py
@@ -0,0 +1,35 @@
+import torch 
+import torch.nn as nn
+
+class StateEncoder(nn.Module):
+    def __init__(self, state_dim, latent_dim=128):
+        super().__init__()
+        self.network = nn.Sequential(
+                nn.Linear(state_dim, 256),
+                nn.ELU(),
+                nn.Linear(256, 256),
+                nn.ELU(),
+                nn.Linear(256, latent_dim)
+        )
+
+    def forward(self, state):
+        return self.network(state)
+        
+
+
+
+class DynamicsModel(nn.Module):
+    def __init__(self, latent_dim, action_dim):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Linear(latent_dim+action_dim, 256),
+            nn.ELU(),
+            nn.Linear(256, 256),
+            nn.ELU(),
+            nn.Linear(256, latent_dim)
+        )
+
+    def forward(self, z, action):
+        x = torch.cat([z, action], dim=-1)
+        return self.network(x)
+        
