76c8d68879faea72c10891f4232229a0fc3a0eb7
44David <david.soane@outlook.com>
2026-07-19 17:31:50 -0600

    implement model class

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import gymnasium as gym
 import panda_gym
 import torch
 
-from model import DynamicsModel, GoalDistanceHead, StateEncoder, SuccessHead
+from model import ReachModel
 from function_helpers import compute_goal_distance, flatten_observation
 
 
@@ -20,36 +20,25 @@ def main():
 
     state = flatten_observation(obs)
 
+    model = ReachModel(state_dim=12, action_dim=3, latent_dim=128)
+
     # add a new dimension for batch (B)
     state_batch = torch.unsqueeze(state, 0)
-
-    state_encoder = StateEncoder(state_dim=12, latent_dim=128)
-    latent_tensor = state_encoder(state_batch)
 
     action_tensor = torch.from_numpy(action)
     action_batch = torch.unsqueeze(action_tensor, 0)
 
-    dynamics_model = DynamicsModel(latent_dim=128, action_dim=3)
-
-    next_latent = dynamics_model(latent_tensor, action_batch)
-
-    goal_dist_head = GoalDistanceHead(latent_dim=128)
-    success_head = SuccessHead(latent_dim=128)
-
-    goal_head_out = goal_dist_head(latent_tensor)
-    success_head_out = success_head(latent_tensor)
-
-    # targets 
-    # the success target can be found in the info variable, its a bool
-
+    outputs = model(state_batch, action_batch)
+    
     goal_distance = compute_goal_distance(obs)
-
-    print(goal_head_out)
-    print(goal_distance)
-    print(success_head_out)
     env_success = info["is_success"]
-    print(env_success)
+    
+    env_success = torch.tensor(env_success).to(torch.float32)
+    
+    goal_distance_float = goal_distance[None, None, ...]
+    goal_distance_float = goal_distance_float.to(torch.float32)
 
+    env_success_float = env_success[None, None, ...]
 
 
 
--- model.py
+++ model.py
@@ -58,3 +58,39 @@ class SuccessHead(nn.Module):
 
     def forward(self, z):
         return self.network(z)
+
+
+class ReachModel(nn.Module):
+    def __init__(self, state_dim, action_dim, latent_dim=128):
+        super().__init__()
+
+        self.encoder = StateEncoder(state_dim, latent_dim)
+        self.dynamics = DynamicsModel(latent_dim, action_dim)
+        self.goal_distance_head = GoalDistanceHead(latent_dim)
+        self.success_head = SuccessHead(latent_dim)
+
+
+    def encode(self, state):
+        return self.encoder(state)
+
+    def predict_next_latent(self, z, action):
+        return self.dynamics(z, action)
+
+    def predict_heads(self, z):
+        return self.goal_distance_head(z), self.success_head(z)
+
+
+    def forward(self, state, action=None):
+        z = self.encode(state)
+        goal_distance, success_logit = self.predict_heads(z)
+
+        if (action is None):
+            return {"z":z, "goal_distance": goal_distance, "success_logit": success_logit}
+
+        else:
+            next_z = self.predict_next_latent(z, action)
+            return {"z":z, "goal_distance": goal_distance, "success_logit": success_logit, "next_z":next_z}
+
+
+        
+            
